4cabf3b6fbc0ee5a1dac0c8615eb07b92c63f198
stacia <razifrizqullah@gmail.com>
2019-07-01 23:53:01 +0700

    CSS integration done

--- ARTIF_project/main/admin.py
+++ ARTIF_project/main/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import artif_start
 # Register your models here.
+admin.site.register(artif_start)
--- ARTIF_project/main/views.py
+++ ARTIF_project/main/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-#from .models import artif_start
+from .models import artif_start
 
 # Create your views here.
 def homepage(request):
-    # return render(request = request, 
-    #                 template_name="homepage/home.html", 
-    #                 context={"Start_app":artif_start.objects.all})
-    return HttpResponse("Wow <strong>LinkGish</strong> is born today!!")
+    return render(request = request, 
+                     template_name='main/home.html',#"/home/linkgish/Desktop/WebApp2/ARTIF_project/ARTIF_project/templates/homepage/home.html", 
+                     context={"artif_start":artif_start.objects.all})
+    #return HttpResponse("Wow <strong>LinkGish</strong> is born today!!")
